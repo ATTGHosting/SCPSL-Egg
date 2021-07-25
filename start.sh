9ba7e0417f3ba87f9b9b6f19d0676f03a65ebbb7
PintTheDragon <PintTheDragon@hotmail.com>
2021-07-24 21:20:24 -0700

    Make auto update outputs nicer

--- start.sh
+++ start.sh
@@ -4,7 +4,16 @@ cd /home/container || exit 1
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
 if [ "${AUTO_UPDATE}" == "true" ]; then
-  ./steam/steamcmd.sh +login anonymous +force_install_dir /home/container/server_files +app_update 996560 validate +quit
+  echo "Updating SCP: Secret Laboratory"
+  steamerr=$( ./steam/steamcmd.sh +login anonymous +force_install_dir /home/container/server_files +app_update 996560 validate +quit 2>&1 > /dev/null)
+
+  if [[ -n "$steamerr" ]]; then
+    echo "\033[31mAn error occurred while updating SCP: Secret Laboratory:\033[m"
+    printf "\033[31m%s\033[m" "$steamerr"
+    exit 1
+  fi
+
+  echo "Successfully updated SCP: Secret Laboratory"
 
   if [ "${INSTALL_EXILED}" == "true" ]; then
     cd server_files || exit 1
@@ -14,7 +23,16 @@ if [ "${AUTO_UPDATE}" == "true" ]; then
       chmod +x ./Exiled.Installer-Linux
     fi
 
-    ./Exiled.Installer-Linux -p /home/container/server_files --appdata /home/container/.config
+    echo "Updating EXILED"
+    exilederr=$( ./Exiled.Installer-Linux -p /home/container/server_files --appdata /home/container/.config 2>&1 > /dev/null)
+
+    if [[ -n "$exilederr" ]]; then
+      echo "\033[31mAn error occurred while updating EXILED:\033[m"
+      printf "\033[31m%s\033[m" "$exilederr"
+      exit 1
+    fi
+
+    echo "Successfully updated EXILED"
   fi
 fi
 
